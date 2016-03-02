31c422713c2f3aab54fc439b2e118733b29335fe
Mike Munsie <mmunsie@researchnow.com>
2016-03-01 22:56:51 -0600

    Classes and objects learning

--- 05 - Classes and Object/ClassesAndObjects.scala
+++ 05 - Classes and Object/ClassesAndObjects.scala
@@ -1,15 +1,29 @@
 object ClassesAndObjects {
 
-  class Point(val x: Double, val y: Double) {
-    def move(dx: Double, dy: Double) = new Point(x + dx, y + dy)
+  // Theses vals are known as instance variables
+  // Note: if we wanted to make this mutatable, we could change the val to a var instead
+  // Name is a field which makes it private :)
+  class Point(name: String, val x: Double, val y: Double) {
+    def move(dx: Double, dy: Double) = new Point("New Point", x + dx, y + dy)
     def distanceFromOrigin = math.sqrt(x * x + y * y)
     override def toString = f"(${x}, ${y})"
   }
 
+  object TestObject {
+    var count = 1
+    def newObject = count+=1
+
+    // Fold has a start value and a function with an accumulator and item
+    def testFold = List("I'm","a","Sentence").fold("Seriously,") { (a, i) => a + f" ${i}" }
+  }
 
   def main(args: Array[String]): Unit = {
-    var p = new Point(3,4)
+    var p = new Point("My Point", 3,4)
     p = p.move(10,0)
-    print(p.toString)
+    println(p.toString)
+    TestObject.newObject
+    TestObject.newObject
+    println(TestObject.testFold)
   }
+
 }
---
+++ 05 - Classes and Object/readme.md
@@ -0,0 +1,5 @@
+Notes:
+
+- Try and make immutable classes as much as possible (don't mutate properties in the class, rather create a new instance)
+- Scala can have private var
+- To create singletons and static methods / variables, use an Object
